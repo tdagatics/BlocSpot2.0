930f969c4cc303f346aeb3a2710d9441c433ee6a
Anthony Dagati <tdagati.cs@gmail.com>
2014-11-12 16:43:21 -0800

    Updated commit

--- BlocSpot2.0/ViewController.h
+++ BlocSpot2.0/ViewController.h
@@ -9,13 +9,27 @@
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h> 
 #import <CoreLocation/CoreLocation.h>
+#import "MapPoint.h"
+
+#define kGOOGLE_API_KEY @"AIzaSyAK-11MCB6KfW6RRb_qXo_DKpaAyF1ybD4"
+#define kBgQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
 
 
 @interface ViewController : UIViewController <MKMapViewDelegate, CLLocationManagerDelegate>
+
+{
+    CLLocationManager *locationManager;
+    CLLocationCoordinate2D currentCentre;
+    int currenDist;
+}
 
 @property (weak, nonatomic) IBOutlet UITextField *searchTextField;
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 - (IBAction)searchButtonPressed:(id)sender;
 
 @end
+
+
+
+
 
--- BlocSpot2.0/ViewController.m
+++ BlocSpot2.0/ViewController.m
@@ -16,6 +16,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    CLLocationCoordinate2D zoomLocation;
+    zoomLocation.latitude = 42.364506;
+    zoomLocation.longitude = -71.057899;
+    
+    
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 1000, 1000);
+    [_mapView setRegion:viewRegion animated:YES];
+    _mapView.showsUserLocation = YES;
+
+    
     // Do any additional setup after loading the view, typically from a nib.
     
     // Make the controller the delegate for the map view
@@ -25,7 +36,6 @@
     [self.mapView setShowsUserLocation:YES];
     
     // Instantiate a location object
-    CLLocationManager *locationManager = [[CLLocationManager alloc] init];
     
     // Make this controller the delegate for the location manager
     [locationManager setDelegate:self];
@@ -37,43 +47,30 @@
 }
 
 - (IBAction)searchButtonPressed:(id)sender {
+    [self.searchTextField resignFirstResponder];
     NSString *searchString = self.searchTextField.text;
     NSLog(@"Just checking: %@", searchString);
     // Use the search string to build the URL query and get data from Google
-//    [self queryGooglePlaces:searchString];
-}
+    MKLocalSearchRequest *request = [[MKLocalSearchRequest alloc] init];
+    request.naturalLanguageQuery = searchString;
+    request.region = _mapView.region;
+    MKLocalSearch *search = [[MKLocalSearch alloc] initWithRequest:request];
+    [search startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
+        for (MKMapItem *item in response.mapItems) {
+            MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+            annotation.title = item.name;
+            annotation.coordinate = item.placemark.coordinate;
+            annotation.subtitle = [NSString stringWithFormat:@"%@, %@ %@",
+                                   item.placemark.addressDictionary[@"Street"],
+                                   item.placemark.addressDictionary[@"State"],
+                                   item.placemark.addressDictionary[@"ZIP"]];
+            [_mapView addAnnotation:annotation];
+        }
+    }];
 
-/*
-
--(void)queryGooglePlaces:(NSString *)googleType {
-    NSString *url = [NSString stringWithFormat:@"https://maps.googleapis.com/maps/api/place/search/json?location=%f,%f&radius=%@&types=%@&sensor=true&key=%@", currentCentre.latitude, currentCentre.longitude, [NSString stringWithFormat:@"%i", currenDist], googleType, kGOOGLE_API_KEY];
     
-    // Formulate the string as an URL object
-    NSURL *googleRequestURL = [NSURL URLWithString:url];
-    
-    // Retrieve the results of the URL
-    dispatch_async(kBgQueue, ^{
-        NSData *data = [NSData dataWithContentsOfURL:googleRequestURL];
-        [self performSelectorOnMainThread:@selector(fetchedData:) withObject:data waitUntilDone:YES];
-        
-    });
-}
-
--(void)fetchedData:(NSData *)responseData {
-    //parse out the json data
-    NSError* error;
-    
-    NSDictionary* json = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&error];
-    
-    // The results from Google will be an array obtained from the NSDictionary object with the key "results"
-    NSArray *places = [json objectForKey:@"results"];
-    
-    // Write out the data to the console.
-    NSLog(@"Google Data: %@", places);
-    [self plotPositions:places];
     
 }
-
 
 #pragma mark - MKMapViewDelegate Methods
 -(void)mapView:(MKMapView *)mapView didAddAnnotationViews:(NSArray *)views {
@@ -97,36 +94,6 @@
     currentCentre = self.mapView.centerCoordinate;
 }
 
--(void)plotPositions:(NSArray *)data {
-    // 1 - Remove any existing custom annotations but not the user location blue dot
-    for (id<MKAnnotation> annotation in _mapView.annotations) {
-        if ([annotation isKindOfClass:[MapPoint class]]) {
-            [_mapView removeAnnotation:annotation];
-        }
-    }
-    // 2 - Loop through the array of placres returned from the Google API.
-    for (int i = 0; i<[data count]; i++) {
-        //Retrieve the NSDictionary object in each index of the array.
-        NSDictionary *place = [data objectAtIndex:i];
-        // 3 - There is a specific NSDictionary object that gives us the location info
-        NSDictionary *geo = [place objectForKey:@"geometry"];
-        // Get the lat and long for the location.
-        NSDictionary *loc = [geo objectForKey:@"location"];
-        // 4 - Get your name and address info for adding to a pin.
-        NSString *name = [place objectForKey:@"name"];
-        NSString *vicinity = [place objectForKey:@"vicinity"];
-        // Create a special variable to hold this coordinate info
-        CLLocationCoordinate2D placeCoord;
-        // Set the lat and long.
-        placeCoord.latitude = [[loc objectForKey:@"lat"] doubleValue];
-        placeCoord.longitude = [[loc objectForKey:@"lng"] doubleValue];
-        // 5 - Create a new annotation
-        MapPoint *placeObject = [[MapPoint alloc] initWithName:name address:vicinity coordinate:placeCoord];
-        [_mapView addAnnotation:placeObject];
-        
-    }
-}
-
 -(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
 {
     // Define your reuse identifier.
@@ -146,10 +113,6 @@
     }
     return nil;
 }
-
-*/
-
-
 
 
 - (void)didReceiveMemoryWarning {
